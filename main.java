e9e889f5c3f719c149a2ee193b66acd44d026a6f
kdelgadop <87090530+kdelgadop@users.noreply.github.com>
2021-08-20 14:18:37 -0400

    files

---
+++ main.java
@@ -0,0 +1,9 @@
+public class main {
+    public static void main(String[] args) {
+
+        NumberGenerator numbers = new NumberGenerator();
+
+        numbers.unRandNum(10, 1);
+        numbers.unRandNum(10, 2);
+    }
+}
